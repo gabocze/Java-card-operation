457bd0cce40de76db6bf2ede1ee959dfc8234172
Gabriel Czernikier <gabocze@gmail.com>
2023-10-11 16:38:33 -0300

    Tarjeta y menú de operaciones.

---
+++ Tarjeta.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Tarjeta {
+    private String marca;
+    private long numero;
+    private String cardholder;
+    private LocalDateTime vencimiento;
+}
