66492b117b9ef6e31541383a93b9d359a20a16eb
Gabriel Czernikier <gabocze@gmail.com>
2023-10-12 22:50:57 -0300

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Java Card Operation
+Ejecutable que procesa operaciones con tarjetas de crédito.
+
+## Instalación con VS Code
+1. Ir a Archivo | agregar carpeta al área de trabajo.
+1. Aceptar la instalación de los plugins sugeridos de Java y de Maven.
+
+## Ejecución
+1. Si no está visible, ir a Ver | Abrir vista
+    1. Escribir Java Project ENTER
+1. Ir a la ventana Java Project
+2. Desplegar el proyecto tarjetarest
+3. Ir al margen derecho del ítem desplegado, hacer clic en el botón Run/Debug
--- src/main/java/tarjetaOp/ConsumoService.java
+++ src/main/java/tarjetaOp/ConsumoService.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 
 public class ConsumoService {
     private Consumo[] consumos = {
-            new Consumo(200, LocalDateTime.of(2023, 1, 16, 
+            new Consumo(200, LocalDateTime.of(2023, 6, 16, 
                     10, 12, 24), 16, new Tarjeta(Marca.VISA, 
                     123456789012L, "CARLOS GARCIA", LocalDateTime.of(2028, 
                     4, 1, 0, 0))),
-            new Consumo(250, LocalDateTime.of(2023, 1, 16,
+            new Consumo(999, LocalDateTime.of(2023, 7, 1,
                     15, 20, 25), 28, new Tarjeta(Marca.VISA,
+                    123456789012L, "CARLOS GARCIA", LocalDateTime.of(2028,
+                    4, 1, 0, 0))),
+            new Consumo(1000, LocalDateTime.of(2023, 7, 5,
+                    8, 40, 25), 28, new Tarjeta(Marca.VISA,
                     123456789012L, "CARLOS GARCIA", LocalDateTime.of(2028,
                     4, 1, 0, 0)))
     };
